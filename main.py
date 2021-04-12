71a56319058bb9e9843ec8d58e3188c106982b6a
a-float <mati852456@gmail.com>
2021-04-12 20:11:24 +0200

    first real commit

---
+++ main.py
@@ -0,0 +1,129 @@
+'''
+Widget animation
+================
+
+This example demonstrates creating and applying a multi-part animation to
+a button widget. You should see a button labelled 'plop' that will move with
+an animation when clicked.
+'''
+
+import kivy
+from kivy.clock import Clock
+from kivy.uix.boxlayout import BoxLayout
+from kivy.app import App
+from kivy.properties import *
+from random import random as r
+from hanoi import hanoi, bicolor_hanoi
+from tower import Tower
+
+kivy.require('2.0.0')
+
+
+class Towers(BoxLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.source = None
+        self.commands = []
+        self.current_disc_count = 3 # no of discs currently on the screen
+        self.current_comm_index = 0
+
+    def init(self, dt):
+        for i in range(self.parent.tower_count):
+            self.add_widget(Tower(i, self.parent.disc_count))
+        self.reset(self.parent.disc_count)  # hardcoded initial values for now
+        return False  # to cancel the clock callback
+
+    def reset(self, disc_count):
+        self.current_disc_count = disc_count
+        for tower in self.children:
+            tower.remove_all_discs()
+            tower.max_discs_no = disc_count
+
+        left_tower = self.children[-1]
+        if self.parent.mode == 'unicolor':
+            get_hue = lambda no: r()
+        else:
+            hue = r()
+            diff = r()
+            get_hue = lambda no: hue if no % 2 else (hue+0.3+diff/3) % 1
+        for i in range(disc_count):
+            left_tower.add_disc(disc_count - i, get_hue(i))
+
+    def init_complete(self, mode):
+        try:
+            if mode == 'unicolor':
+                hanoi(*reversed(self.children), self.parent.disc_count, self.commands)
+            elif mode == 'bicolor':
+                bicolor_hanoi(*reversed(self.children), self.parent.disc_count, self.commands)
+            print("Starting the solve")
+            self.step()
+        except AssertionError as e:
+            print("Could not start the solve: " + str(e))
+
+    def step(self):
+        if self.commands:
+            comm = self.commands[self.current_comm_index]
+            self.current_comm_index += 1
+            self.parent.move_count += 1
+            comm[0].move_disc_to(comm[1])
+            if self.current_comm_index == len(self.commands):  # it's the last command
+                self.stop_complete()
+
+    def stop_complete(self):
+        self.commands = []
+        self.current_comm_index = 0
+        print("The solve has been completed")
+
+    def on_touch_down(self, touch):
+        for i, child in enumerate(self.children):
+            if child.collide_point(*touch.pos):
+                print(f"Ah im touched ,,^^,, {i}")
+                if self.source is None:
+                    self.source = child
+                    self.source.toggle_selected()
+                elif self.source == child:
+                    self.source.toggle_selected()
+                    self.source = None
+                else:
+                    self.source.move_disc_to(child)
+                    self.parent.move_count += 1
+                    self.source.toggle_selected()
+                    self.source = None
+
+
+class Controller(BoxLayout):
+    move_count = NumericProperty(0)
+    disc_count = NumericProperty(3)
+    mode = StringProperty('unicolor')
+    towers_object = ObjectProperty()
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.tower_count = 3
+        Clock.schedule_interval(self.towers_object.init, 1)  # replace here
+
+    def reset(self):
+        self.move_count = 0
+        self.towers_object.reset(self.disc_count)
+
+    def complete(self):
+        if self.towers_object.current_disc_count != self.disc_count:
+            self.reset()
+        for child in self.towers_object.children[:-1]:
+            if child.discs:
+                self.reset()
+                break
+        self.towers_object.init_complete(self.mode)
+
+    def stop_complete(self):
+        if self.towers_object.commands:
+            self.towers_object.stop_complete()
+
+
+class HanoiApp(App):
+    def build(self):
+        return Controller()
+
+
+if __name__ == '__main__':
+    HanoiApp().run()
---
+++ tower.py
@@ -0,0 +1,93 @@
+from kivy.animation import Animation
+from kivy.uix.widget import Widget
+from kivy.graphics import Color, Rectangle
+
+
+class Tower(Widget):
+    def __init__(self, index, max_discs_no, **kwargs):
+        super().__init__(**kwargs)
+        self.index = index
+        self.discs = []
+        self.disc_data = []
+        self.max_discs_no = max_discs_no  # == max radius
+        self.target_tower = None
+        self.select_marker = None
+        self.disc_height = lambda: min((self.height * 0.06, self.height*0.53/self.max_discs_no))
+        self.get_disc_y_pos = lambda n: self.height * 0.22 + n * self.disc_height()
+        # Arranging Canvas
+        with self.canvas:
+            Color(1, 1, 0)  # set the pole color
+            # Setting the size and position of canvas
+            self.rod = Rectangle()
+            self.rod_base = Rectangle()
+            # Make the tower responsive
+            self.bind(size=self._update_tower)
+
+    # update function which makes the canvas adjustable.
+    def _update_tower(self, *args):
+        self.rod.pos = (self.center_x - self.width / 200, self.height * 0.2)
+        self.rod.size = (self.width / 100, self.height * 0.6)
+        self.rod_base.pos = (self.center_x - self.width * 0.4, self.height * 0.2)
+        self.rod_base.size = (self.width * 0.8, self.height * 0.02)
+
+        for i in range(len(self.discs)):
+            self._update_disc(i, self.disc_data[i][0])
+
+        if self.select_marker:
+            self.select_marker.pos = (self.center_x - self.width * 0.3, self.height * 0.1)
+            self.select_marker.size = (self.width * 0.6, self.height * 0.07)
+
+    def add_disc(self, radius, hue):
+        with self.canvas:
+            Color(hue, 1, 1, mode='hsv')
+            disc = Rectangle()
+            self.discs.append(disc)
+            self._update_disc(len(self.discs) - 1, radius)
+            self.disc_data.append((radius, hue))
+
+    def toggle_selected(self):
+        if not self.select_marker:
+            with self.canvas:
+                Color(0.5, 0.5, 0)
+                self.select_marker = Rectangle()
+            self._update_tower()
+        else:
+            self.canvas.remove(self.select_marker)
+            self.select_marker = None
+
+    def _update_disc(self, index, radius):
+        disc_width = self.width * 0.7 * radius / self.max_discs_no
+        disc = self.discs[index]
+        disc.pos = (self.center_x - disc_width * 0.5,
+                    self.get_disc_y_pos(index))
+        disc.size = (disc_width, self.disc_height())
+
+    def remove_disc(self):
+        if len(self.discs) == 0:
+            raise IndexError('Tried to remove a disc from an empty tower')
+        self.canvas.remove(self.discs.pop())
+        self.disc_data.pop()
+
+    def remove_all_discs(self):
+        while self.discs:
+            self.remove_disc()
+
+    def _on_move_complete(self, instance, widget):
+        self.target_tower.add_disc(*self.disc_data[-1])
+        self.remove_disc()
+        self.parent.step()
+
+    def move_disc_to(self, target_tower):
+        if not self.discs:
+            print("Cant move a disc out of an empty tower :C")
+            return
+        disc = self.discs[-1]
+        self.target_tower = target_tower
+        dur = 0.05
+        index_diff = target_tower.index - self.index
+        anim = Animation(pos=(disc.pos[0], self.height * 0.85), duration=dur)
+        anim += Animation(pos=(disc.pos[0] + index_diff * self.width, self.height * 0.85), duration=dur)
+        anim += Animation(pos=(disc.pos[0] + index_diff * self.width, self.get_disc_y_pos(len(target_tower.discs))),
+                          duration=dur)
+        anim.bind(on_complete=self._on_move_complete)
+        anim.start(disc)
